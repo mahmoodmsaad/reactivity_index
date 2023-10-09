8c478ffc1093c3f27d23c9a8ae5c3ae8cfb136c3
saad <146743997+mahmoodmsaad@users.noreply.github.com>
2023-10-09 20:40:38 +0500

    Update reactivity.py

--- reactivity.py
+++ reactivity.py
@@ -4,12 +4,15 @@ from calculate_reactivity import calculate_reactivity_indices
 def main():
     st.title("Reactivity Indices Calculator")
 
-    # User inputs in hetree units
-    homo_hetree = st.number_input("Enter HOMO value in hetree:")
-    lumo_hetree = st.number_input("Enter LUMO value in hetree:")
+    # User inputs
+    homo_ev = st.number_input("Enter HOMO value in eV:", value=0.0, step=0.01)
+    lumo_ev = st.number_input("Enter LUMO value in eV:", value=0.0, step=0.01)
+
+    if homo_ev < 0 or lumo_ev < 0:
+        st.warning("Negative values are ignored. Please enter positive values for HOMO and LUMO.")
 
     # Calculate reactivity indices
-    reactivity_indices = calculate_reactivity_indices(homo_hetree, lumo_hetree)
+    reactivity_indices = calculate_reactivity_indices(homo_ev, lumo_ev)
 
     # Display the results
     st.header("Reactivity Indices:")
