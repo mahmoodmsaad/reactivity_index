06cb62f0fbc5be7803b5579064111c4fe7b8df35
saad <146743997+mahmoodmsaad@users.noreply.github.com>
2023-10-09 15:20:01 +0500

    Update calculate_reactivity.py

--- calculate_reactivity.py
+++ calculate_reactivity.py
@@ -1,7 +1,20 @@
-# calculate_reactivity.py
 def calculate_reactivity_indices(homo_ev, lumo_ev):
     # Calculate delta_E in eV
     delta_e = lumo_ev - homo_ev
+
+    if delta_e == 0:
+        # Handle the case where delta_e is zero to avoid division by zero
+        return {
+            "HOMO (eV)": homo_ev,
+            "LUMO (eV)": lumo_ev,
+            "ΔE (eV)": delta_e,
+            "Electronegativity (χ)": float('nan'),
+            "Chemical Potential (µ)": float('nan'),
+            "Hardness (η)": float('nan'),
+            "Electrophilicity (ɷ)": float('nan'),
+            "Softness (S)": float('nan'),
+            "Total amount of charge transfer (δN)": float('nan'),
+        }
 
     # Calculate electronegativity, chemical potential, hardness, electrophilicity, and softness
     electronegativity = (homo_ev + lumo_ev) / 2
