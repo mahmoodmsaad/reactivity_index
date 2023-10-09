48111fecc509016fd2cfd90adc106fdce873c00b
saad <146743997+mahmoodmsaad@users.noreply.github.com>
2023-10-09 15:29:11 +0500

    Update reactivity.py

--- reactivity.py
+++ reactivity.py
@@ -1,16 +1,15 @@
-# app.py
 import streamlit as st
 from calculate_reactivity import calculate_reactivity_indices
 
 def main():
     st.title("Reactivity Indices Calculator")
 
-    # User inputs
-    homo_ev = st.number_input("Enter HOMO value in eV:")
-    lumo_ev = st.number_input("Enter LUMO value in eV:")
+    # User inputs in hetree units
+    homo_hetree = st.number_input("Enter HOMO value in hetree:")
+    lumo_hetree = st.number_input("Enter LUMO value in hetree:")
 
     # Calculate reactivity indices
-    reactivity_indices = calculate_reactivity_indices(homo_ev, lumo_ev)
+    reactivity_indices = calculate_reactivity_indices(homo_hetree, lumo_hetree)
 
     # Display the results
     st.header("Reactivity Indices:")
