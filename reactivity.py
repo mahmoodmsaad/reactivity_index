bef7941c7dff263cfffb3cd1150c40f264ffc216
saad <146743997+mahmoodmsaad@users.noreply.github.com>
2023-10-09 14:43:02 +0500

    reactivity.py

---
+++ reactivity.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from calculate_reactivity import calculate_reactivity_indices
+
+def main():
+    st.title("Reactivity Indices Calculator")
+
+    # User inputs
+    homo_ev = st.number_input("Enter HOMO value in eV:")
+    lumo_ev = st.number_input("Enter LUMO value in eV:")
+
+    # Calculate reactivity indices
+    reactivity_indices = calculate_reactivity_indices(homo_ev, lumo_ev)
+
+    # Display the results
+    st.header("Reactivity Indices:")
+    for key, value in reactivity_indices.items():
+        st.write(f"{key}: {value:.4f} eV")
+
+if __name__ == "__main__":
+    main()
